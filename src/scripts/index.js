c071df8da86b911f23be7d12e46d4ed8bbb2e911
bloxboy <frederic.bloxboy@gmail.com>
2021-05-20 17:04:20 +0200

    [CHG] fix zoom windows

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,6 @@
 //import './accordion'
 //import './pricing'
 import './heroslid'
+
+document.querySelector("meta[name=viewport]").setAttribute('content', 'width=device-width, initial-scale='+(1/window.devicePixelRatio));
+console.log(window.devicePixelRatio);
