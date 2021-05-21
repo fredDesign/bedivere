94bb457d3668c4f7bdb15ccb0908c339ad636aac
bloxboy <frederic.bloxboy@gmail.com>
2021-05-21 12:10:35 +0200

    [CHG] remove pixel ratio

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,5 +2,7 @@
 //import './pricing'
 import './heroslid'
 
-document.querySelector("meta[name=viewport]").setAttribute('content', 'width=device-width, initial-scale='+(1/window.devicePixelRatio));
+//*
+// document.querySelector("meta[name=viewport]").setAttribute('content', 'width=device-width, initial-scale='+(1/window.devicePixelRatio));
+//
 console.log(window.devicePixelRatio);
